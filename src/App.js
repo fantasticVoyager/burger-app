3abcc3f9144ff2be285a36e03e631e27a09100a5
Keith Rasweiler <saintmarzano@gmail.com>
2020-06-16 14:05:36 -0400

    added aux component and Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
         <p>it still works</p>
+        <Layout>
+          <p>stuff in a paragraph</p>
+          <div>stuff in a div</div>
+        </Layout>
       </div>
     );
   }
